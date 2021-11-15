b77adf7a02456f1a25910e0899c741887872b4b1
anonomity <jlauravelez@gmail.com>
2021-11-15 12:11:01 +0100

    perlin noise

---
+++ Assets/Scripts/Camera/MouseCamLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamLook : MonoBehaviour
+{
+	[SerializeField]
+	public float sensitivity = 100f;
+
+	public GameObject character;
+
+	float xRotation = 0f;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		character = this.transform.parent.gameObject;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		character.transform.Rotate(Vector3.up * mouseX);
+	}
+}
---
+++ Assets/Scripts/WorldGeneration/WorldGen.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour
+{
+	[SerializeField]
+
+	public int width;
+	public int height;
+	public float scale;
+
+	private float noiseScale = 1f;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		for (int x = 0; x < width; x++)
+		{
+			for (int z = 0; z < width; z++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+
+
+
+					if ((Perlin3D(x, y) * (float)height) > y)
+					{
+						Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
+					}
+				}
+			}
+
+		}
+
+	}
+
+	public float Perlin3D(int x, int y)
+	{
+		float Nwidth = (float)width;
+		float Nheight = (float)height;
+		float newx = (float)x;
+		float newy = (float)y;
+
+		float X = (float)(newx / Nwidth);
+		float Y = (float)(newy / Nwidth);
+
+
+		float t = Mathf.PerlinNoise(X * scale, Y * scale);
+		// Debug.Log(t);
+		return t;
+
+
+
+
+	}
+
+}
